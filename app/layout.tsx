8492e83b4f07b030ab085ddc4cc91ea824a181b4
Baylee Schmeisser <github.mx@baylee.dev>
2024-09-29 10:29:44 -0700

    🌿 Chapter 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import { sac } from '@/app/ui/fonts';
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -8,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${sac.className} antialiased`}>
+      <body className={`${inter.className} antialiased`}>
         {children}
       </body>
     </html>
